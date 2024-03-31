20d20baba700d5768a200dd2b18fde8c9b6038b4
crazyplayy <afa.santos44@gmail.com>
2024-03-31 15:01:11 +0100

    Add logo & toolbar, extend search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Input, Tooltip } from "antd";
+import { QuestionCircleOutlined } from "@ant-design/icons";
 import { usePokemonContext } from "../context/PokemonContext";
+import Pokelogo from "../assets/Pokelogo.png";
 
 const { Search } = Input;
 
@@ -14,16 +16,27 @@ const SearchBar = () => {
   };
 
   return (
-    <div>
-      <Search
-        placeholder="Which Pokemon are you looking for?"
-        allowClear
-        enterButton="Search"
-        size="large"
-        onSearch={handleSearch}
-        onChange={(event) => setSearchText(event.target.value)}
-        value={searchText}
-      />
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <img src={Pokelogo} alt="Logo" style={{ width: 200, marginBottom: 20 }} />
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Search
+          placeholder="Who are you looking for?"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={handleSearch}
+          onChange={(event) => setSearchText(event.target.value)}
+          value={searchText}
+          style={{ width: 400}}
+        />
+        <Tooltip title=<div>Search for a Pokémon <br/>  <strong>name</strong> or <strong> number</strong></div>>
+          <QuestionCircleOutlined
+            style={{ fontSize: 15, color: "gray", marginLeft: 10 }}
+          />
+        </Tooltip>
+      </div>
     </div>
   );
 };
